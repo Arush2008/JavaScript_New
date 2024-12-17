74bf31b4b2de0a485a7dbdf87c6150945681994e
Arush2008 <23779@burnside.school.nz>
2024-12-18 02:23:30 +1300

    12th lesson Part 1

---
+++ script/12-todo-list.js
@@ -0,0 +1,56 @@
+const todoList = [
+{name: 'Learn coding', 
+    dueDate: '25-08-2025'
+}, 
+{name: 'Earn some money',
+    dueDate: '25-08-2025'
+    }];
+
+renderTodoList ();
+
+function renderTodoList () {
+
+let todoListHTML = '';
+
+todoList.forEach(function(todoObject, index) {
+    const todoObject = todoList[i];
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+    const {name} = todoObject;
+    const {dueDate} = todoObject;
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();"
+        class="delete-input"
+    > Delete </button>
+    `;
+
+    todoListHTML += html;
+})
+
+document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input') ;
+    const name = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-due-date-input') ;
+    const dueDate = dateInputElement.value ;
+
+    todoList.push({
+        // name: name,
+        // dueDate: dueDate
+        name,
+        dueDate
+});
+
+    inputElement.value = '';
+
+    renderTodoList();
+}
